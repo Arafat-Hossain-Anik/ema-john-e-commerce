e0e6050acac6dce79b265658ac097a56fed6dde3
Arafat-Hossain-Anik <arafat15@cse.pstu.ac.bd>
2021-10-01 16:04:17 +0600

    fetch products by externel function

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 import './OrderReview.css';
 const OrderReview = () => {
+    const [products] = useProducts();
     return (
         <div>
+            <h1>{products.length}</h1>
             <h1>this is order review page</h1>
         </div>
     );
